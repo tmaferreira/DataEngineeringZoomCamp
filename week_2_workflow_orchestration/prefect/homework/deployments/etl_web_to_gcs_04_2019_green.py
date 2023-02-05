f54356bab25c2b4b7879bc6e0125d74c4c934d98
tmaferreira <taniamarge@gmail.com>
2023-02-05 15:50:59 +0000

    Homework 2 resolutions

--- week_2_workflow_orchestration/prefect/homework/deployments/etl_web_to_gcs_04_2019_green.py
+++ week_2_workflow_orchestration/prefect/homework/deployments/etl_web_to_gcs_04_2019_green.py
@@ -11,6 +11,17 @@ def fetch(dataset_url: str) -> pd.DataFrame:
     return df
 
 
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
@@ -28,18 +39,19 @@ def write_gcs(path: Path) -> None:
 
 
 @flow()
-def etl_web_to_gcs() -> None:
+def etl_web_to_gcs_q5() -> None:
     """The main ETL function"""
     color = "green"
-    year = 2020
-    month = 11
+    year = 2019
+    month = 4
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
-    path = write_local(df, color, dataset_file)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
 
 
 if __name__ == "__main__":
-    etl_web_to_gcs()
+    etl_web_to_gcs_q5()
--- week_2_workflow_orchestration/prefect/homework/deployments/github_deploy_q5.py
+++ week_2_workflow_orchestration/prefect/homework/deployments/github_deploy_q5.py
@@ -1,5 +1,5 @@
 from prefect.deployments import Deployment
-from etl_web_to_gcs_11_2020_green import etl_web_to_gcs
+from etl_web_to_gcs_04_2019_green import etl_web_to_gcs_q5
 from prefect.filesystems import GitHub
 
 github_block = GitHub.load("github-connector")
@@ -8,7 +8,7 @@ github_deploy = Deployment.build_from_flow(
     flow=etl_web_to_gcs,
     name="github-flow",
     storage=github_block,
-    entrypoint="/homework/deployments/etl_web_to_gcs_11_2020_green.py:etl_web_to_gcs"
+    entrypoint="/homework/deployments/etl_web_to_gcs_04_2019_green.py:etl_web_to_gcs_q5"
 )
 
 if __name__ == "__main__":
